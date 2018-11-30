e5f8bda289294077f0c23dd157c14f445272e534
stanford <stanfordlin@outlook.com>
2018-11-30 00:19:20 -0800

    Typo and make build

--- web/components/local-hack-day/Questions.js
+++ web/components/local-hack-day/Questions.js
@@ -123,7 +123,7 @@ export const QUESTIONS = Object.freeze({
         'Can I work on my previous projects?': (
           <p className="margin-top-none">
             No, all projects must be built from scratch. However, you are welcome to brainstorm
-            ideas beforehand. This rule is strictly adhered to at nwHacks and any team found
+            ideas beforehand. This rule is strictly adhered to at UBC Local Hack Day and any team found
             working on previous projects will be automatically disqualified in the event. If
             you are using libraries or tools that you have made in the past, don&apos;t worry, this
             does not apply.
